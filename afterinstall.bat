c1b4fa424658b7d081edd532a2d68ce4660d0a68
luiszuniga1980 <32581431+luiszuniga1980@users.noreply.github.com>
2020-10-25 19:26:23 -0500

    Update afterinstall.bat

--- afterinstall.bat
+++ afterinstall.bat
@@ -1,1 +1,9 @@
+mkdir C:\TestResults
+mkdir C:\TestResults\All
+mkdir C:\TestResults\Delta
+mkdir C:\TestResults\ThePeople
+mkdir C:\TestResults\ChrimsonHawks
+mkdir C:\TestResults\Newton
+mkdir C:\TestResults\Synergy
+
 powershell C:\Epsilon\Automation\Epsilon.Fusion.Automation\AutomationExecution.ps1
