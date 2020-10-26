28fdc2ea553c5a86a9c2079eaddb4866c33b0be5
luiszuniga1980 <32581431+luiszuniga1980@users.noreply.github.com>
2020-10-25 19:52:14 -0500

    Update afterinstall.bat

--- afterinstall.bat
+++ afterinstall.bat
@@ -2,7 +2,7 @@ mkdir C:\TestResults
 mkdir C:\TestResults\All
 mkdir C:\TestResults\Delta
 mkdir C:\TestResults\ThePeople
-mkdir C:\TestResults\ChrimsonHawks
+mkdir C:\TestResults\CrimsonHawks
 mkdir C:\TestResults\Newton
 mkdir C:\TestResults\Synergy
 
